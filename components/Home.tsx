f08eeab8dbda4363c52b0199013517a42ac75772
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-09-18 04:58:04 +0530

    wagmi: bug fixes & improvements

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
     <section>
       {/* pokemon graphql server https://graphql-pokeapi.graphcdn.app/ */}
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
